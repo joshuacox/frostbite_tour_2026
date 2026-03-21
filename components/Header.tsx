76d3ee136fd041a60868fd4480dd2bda3fa237f3
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-03-20 22:41:54 -0500

    newline

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,6 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
-
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
   if (siteMetadata.stickyNav) {

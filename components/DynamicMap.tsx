372e001565a9e801afbf83a9695184f97e8d1531
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-03-30 11:54:59 -0500

    Now having hydration issues, but I think I worked around the mdx import stuff

---
+++ components/DynamicMap.tsx
@@ -0,0 +1,12 @@
+// components/DynamicMap.tsx
+import dynamic from 'next/dynamic';
+
+const DynamicMap = dynamic(
+  () => import('./Map'), // Your actual Leaflet map component
+  {
+    ssr: false,
+    loading: () => <p>Loading Map...</p>
+  }
+);
+
+export default DynamicMap;
